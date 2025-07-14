0f20489430a9ab0eca8ced3b6d66b680a57da54f
Salmanahmed22 <123300970+Salmanahmed22@users.noreply.github.com>
2025-07-14 19:59:59 +0300

    added quesrion service

---
+++ questionService/src/test/java/org/codewithsoly/questionservice/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codewithsoly.questionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
